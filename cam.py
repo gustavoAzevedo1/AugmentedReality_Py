ab36654241b26befe5c5418aa75accf6fc320fab
Gustavo Armborst Guedes de Azevedo <gustavo.hagazevedo@gmail.com>
2018-07-05 19:21:45 -0300

    update

--- cam.py
+++ cam.py
@@ -2,12 +2,18 @@ import cv2
 import numpy as np
 import random
 
-MIN_MATCH_COUNT = 30
+MIN_MATCH_COUNT = 15
 
 detector = cv2.xfeatures2d.SIFT_create()
 
 FRAME = 0
 
+FRAMEMAX = 1000
+MOVIMENTO = 10
+DENSIDADE = 5
+ESPEC = 2
+
+THRMASK = 10
 TAMANHOGRM = 250
 
 FLANN_INDEX_KDITREE = 0
@@ -16,7 +22,7 @@ flann = cv2.FlannBasedMatcher(flannParam, {})
 
 gramaImg = []
 lista = []
-trainImg = cv2.imread("logo.jpg", 0)
+trainImg = cv2.imread("logo5.jpg", 0)
 trainImg = cv2.flip(trainImg, 1)
 trainKP, trainDesc = detector.detectAndCompute(trainImg, None)
 
@@ -50,7 +56,7 @@ def show_webcam(mirror=False):
                 trainBorder = np.float32([[[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]])
                 queryBorder = cv2.perspectiveTransform(trainBorder, H)
                 # cv2.polylines(img, [np.int32(queryBorder)], True, (0, 255, 0), 5)
-                img, frame = gramaWeb(img, trainBorder, frame)
+                img, frame = gramaWeb(img, queryBorder, frame)
             cv2.imshow('webcam', img)
         if cv2.waitKey(1) == 27:
             break  # esc para sair
@@ -65,74 +71,70 @@ def calculaBezier(aws, p1, p2, p3, p4):
             + aws ** 3 * p4)
 
 
-def grama():
-    x = 5
-    while x < 230:
-        lista2 = []
-        lista2.append([x, (TAMANHOGRM - 200)])
-        lista2.append([x, (TAMANHOGRM - 100)])
-        lista2.append([x, (TAMANHOGRM - 50)])
-        lista2.append([x, (TAMANHOGRM - 0)])
-        lista.append(lista2)
-        x = x + 10
-
-
-def gramado():
-    x = 0
-    while x < 500:
-        for i in range(len(lista)):
-            aws = np.zeros((TAMANHOGRM, TAMANHOGRM), np.uint8)
-            aws2 = lista[i]
-            aws2[-4][0] = aws2[-4][0] + random.randrange(-20, 20)
-            aws2[-3][0] = aws2[-3][0] + random.randrange(-20, 20)
-            gramaImg.append(desenha(aws, aws2))
-        x = x + 1
-
-
 def gramaWeb(img, borda, frame):
-    if frame > 500:
+    if frame > FRAMEMAX:
         frame = 0
-
-    rows, cols, ch = TAMANHOGRM, TAMANHOGRM, 3
+    im = gramaImg[frame]
+    rows, cols, ch = im.shape
     pts1 = np.float32([[0, 0], [cols, 0], [cols, rows], [0, rows]])
-    # pts2 is used for defining the perspective transform
-    # pts2 = np.float32([borda[0][0], borda[0][3], borda[0][2], borda[0][1]])
-    # compute the transform matrix
-    M = cv2.getPerspectiveTransform(pts1, borda)
-    # M = borda
+
+    pts2 = np.float32([borda[0][0], borda[0][3], borda[0][2], borda[0][1]])
+
+    M = cv2.getPerspectiveTransform(pts1, pts2)
     rows, cols, ch = img.shape
-    # make the perspective change in a image of the size of the camera input
-    dst = cv2.warpPerspective(gramaImg[frame], M, (cols, rows))
-    # A mask is created for adding the two images
-    # maskThreshold is a variable because that allows to substract the black background from different images
-    ret, mask = cv2.threshold(cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY), 10, 1, cv2.THRESH_BINARY_INV)
-    # Erode and dilate are used to delete the noise
+
+    dst = cv2.warpPerspective(im, M, (cols, rows))
+
+    ret, mask = cv2.threshold(cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY), THRMASK, 1, cv2.THRESH_BINARY_INV)
+
     mask = cv2.erode(mask, (3, 3))
     mask = cv2.dilate(mask, (3, 3))
-    # The two images are added using the mask
+
     for c in range(0, 3):
         img[:, :, c] = dst[:, :, c] * (1 - mask[:, :]) + img[:, :, c] * mask[:, :]
-
-    # novoArray = np.float32([borda[0][0], borda[0][3], borda[0][2], borda[0][1]])
-    # resultado = cv2.getPerspectiveTransform(np.float32(gramaImg[frame]), novoArray)
-    # resultadoImg = cv2.warpPerspective(gramaImg[frame], resultado, (640, 480))  # resolucao da minha web cam
-    # resultadoFinalImg = cv2.addWeighted(img, 1, resultadoImg, 2, 0)
 
     frame = frame + 1
     return img, frame
 
 
-def desenha(aws, lista):
+def grama():
+    x = 0
+    while x < TAMANHOGRM:
+        lista2 = []
+        lista2.append([x, (TAMANHOGRM - 150)])
+        lista2.append([x, (TAMANHOGRM - 100)])
+        lista2.append([x, (TAMANHOGRM - 50)])
+        lista2.append([x, (TAMANHOGRM - 0)])
+        lista.append(lista2)
+        x = x + DENSIDADE
+
+
+def gramado():
+    x = 0
+    while x < FRAMEMAX:
+        aws = np.zeros((TAMANHOGRM, TAMANHOGRM, 3), np.uint8)
+        for i in range(len(lista)):
+            aws2 = lista[i]
+            aws2[0][0] = aws2[-4][0] + random.randrange(-MOVIMENTO, MOVIMENTO)
+            aws2[1][0] = aws2[-3][0] + random.randrange(-MOVIMENTO, MOVIMENTO)
+            aws = desenha(aws, aws2)
+
+        gramaImg.append(cv2.flip(aws, 0))
+        x = x + 1
+
+
+def desenha(aws1, aws2):
+    aws = aws1
     t = np.arange(0, 1.1, 0.1)
     lista2 = []
     for i in range(len(t)):
         ts = float(format(t[i], '.2f'))
-        x = calculaBezier(ts, lista[-4][0], lista[-3][0], lista[-2][0], lista[-1][0])
-        y = calculaBezier(ts, lista[-4][1], lista[-3][1], lista[-2][1], lista[-1][1])
+        x = calculaBezier(ts, aws2[0][0], aws2[1][0], aws2[2][0], aws2[3][0])
+        y = calculaBezier(ts, aws2[0][1], aws2[1][1], aws2[2][1], aws2[3][1])
         pts = (x, y * -1 + TAMANHOGRM)
         lista2.append(pts)
     pts = np.array(lista2, np.int32)
-    cv2.polylines(aws, [pts], False, (0, 255, 0), 2, 8)
+    cv2.polylines(aws, [pts], False, (0, 250, 0), ESPEC, 2)
     return aws
 
 
--- grama.py
+++ grama.py
@@ -16,53 +16,42 @@ def calculaBezier(aws, p1, p2, p3, p4):
 
 
 def grama():
-    x = 5
+    x = 21
     while x < 230:
         lista2 = []
-        lista2.append([x, (TAMANHOGRM - 200)])
+        lista2.append([x, (TAMANHOGRM - 150)])
         lista2.append([x, (TAMANHOGRM - 100)])
         lista2.append([x, (TAMANHOGRM - 50)])
         lista2.append([x, (TAMANHOGRM - 0)])
         lista.append(lista2)
-        x = x + 10
+        x = x + 5
 
 
 def gramado():
     x = 0
     while x < 500:
+        aws = np.zeros((TAMANHOGRM, TAMANHOGRM, 3), np.uint8)
         for i in range(len(lista)):
-            aws = np.zeros((TAMANHOGRM, TAMANHOGRM), np.uint8)
             aws2 = lista[i]
-            aws2[-4][0] = aws2[-4][0] + random.randrange(-20, 20)
-            aws2[-3][0] = aws2[-3][0] + random.randrange(-20, 20)
-            gramaImg.append(desenha(aws, aws2))
+            aws2[0][0] = aws2[-4][0] + random.randrange(-20, 20)
+            aws2[1][0] = aws2[-3][0] + random.randrange(-20, 20)
+            aws = desenha(aws, aws2)
+
+        gramaImg.append(cv2.flip(aws, 0))
+        # cv2.imshow("sdas", aws)
         x = x + 1
 
 
-def desenha(aws, lista):
+def desenha(aws1, aws2):
+    aws = aws1
     t = np.arange(0, 1.1, 0.1)
     lista2 = []
     for i in range(len(t)):
         ts = float(format(t[i], '.2f'))
-        x = calculaBezier(ts, lista[-4][0], lista[-3][0], lista[-2][0], lista[-1][0])
-        y = calculaBezier(ts, lista[-4][1], lista[-3][1], lista[-2][1], lista[-1][1])
+        x = calculaBezier(ts, aws2[0][0], aws2[1][0], aws2[2][0], aws2[3][0])
+        y = calculaBezier(ts, aws2[0][1], aws2[1][1], aws2[2][1], aws2[3][1])
         pts = (x, y * -1 + TAMANHOGRM)
         lista2.append(pts)
     pts = np.array(lista2, np.int32)
-    cv2.polylines(aws, [pts], False, (0, 255, 0), 2, 8)
+    cv2.polylines(aws, [pts], False, (0, 255, 0), 2, 2)
     return aws
-
-def show():
-    while True:
-        cv2.imshow("sdas", gramaImg[120])
-        if cv2.waitKey(1) == 27:
-            break
-    cv2.destroyAllWindows()
-
-def main():
-    grama()
-    gramado()
-    show()
-
-if __name__ == '__main__':
-    main()
